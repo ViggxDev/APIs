32b4057dba9bb984e276face106c0825b92cbec4
= <viggo458@gmail.com>
2023-02-08 20:34:11 +0100

    First Commit

---
+++ main.py
@@ -0,0 +1,43 @@
+#Comments = input type
+
+#Errors
+def err_list():
+    return "There was an error executing your command"
+
+#Lists
+#d = digits
+def d(l):
+    for x in l:
+        if not str(x).isdigit():
+            return False
+    return True
+
+#rd = remove duplicates
+def rd(l):
+    for x in l:
+        if l.count(x) >= 2:
+            l.remove(x)
+    return l
+
+#Sorted list (increasing) without duplicates
+def srd_i(l):
+    return sorted(rd(l))
+
+#Sorted list (decreasing) without duplicates
+def srd_d(l):
+    return sorted(rd(l))[::-1]
+
+#Sorted list (increasing)
+def si(l):
+    return sorted(l)
+
+#Sorted list (decreasing)
+def sd(l):
+    return sorted(l)[::-1]
+
+#First target --> return index of first element
+def ft(l, t):
+    for i,e in enumerate(l):
+        if e == t:
+            return i
+    return err_list()
---
+++ tester.py
@@ -0,0 +1,3 @@
+from main import *
+
+print(ft([1,2,3,4,3,2,5,2], 6))
